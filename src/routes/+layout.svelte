a1b8ea055d0ce214fbfa2b3c7a66b1893c4a2b64
Jericho de Guzman <poloman7190@gmail.com>
2024-02-05 17:04:36 +0000

    setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+	import SideBarIcon from './SideBarIcon.svelte';
+</script>
+
+<div
+	class="fixed top-0 left-0 h-screen w-32 m-0 flex flex-col bg-gray-100 text-white shadow-lg"
+>
+    <SideBarIcon text="banana" />
+</div>
+
+<slot />
---
+++ src/routes/SideBarIcon.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let text;
+</script>
+
+<div class="sidebar-icon group">
+	<span class="sidebar-tooltip group-hover:opacity-100">
+		{text}
+	</span>
+</div>
