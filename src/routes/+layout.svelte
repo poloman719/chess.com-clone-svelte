d44bfb11bbd1630179e595f5253906a266e3d478
Jericho de Guzman <poloman7190@gmail.com>
2024-02-11 20:09:04 +0000

    trying to make a working nav menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,25 @@
 	import '../app.css';
 	import NavItem from './NavItem.svelte';
 
-	const navItems = [{icon: "♟️", title: "Play"}]
+	let selected = "nothing selected";
+
+	const navItems = [
+		{ icon: '♟️', title: 'Play' },
+		{ icon: '🧩', title: 'Puzzle' },
+		{ icon: '🎓', title: 'Learn' },
+		{ icon: '🥽', title: 'Watch' },
+		{ icon: '📰', title: 'News' },
+		{ icon: '🫂', title: 'Social' },
+		{ icon: '😀', title: 'More' }
+	];
 </script>
 
 <div class="flex">
 	<div class="h-screen w-40 m-0 flex flex-col bg-gray-100 text-white shadow-lg">
-		<NavItem icon="♟️" title="Play" />
-		<NavItem icon="🧩" title="Puzzles" />
-		<NavItem icon="🎓" title="Learn" />
-		<NavItem icon="🥽" title="Watch" />
-		<NavItem icon="📰" title="News" />
-		<NavItem icon="🫂" title="Social" />
-		<NavItem icon="😀" title="More" />
+		{#each navItems as item}
+			<NavItem icon={item.icon} title={item.title} />
+		{/each}
+		<div>{selected}</div>
 	</div>
 	<div class="bg-gray-25 fixed left-40 w-80 h-screen"></div>
 	<div class="bg-gray-200 flex-grow">
