47b631d77acee453540166cce43ab92c3a24b7ce
poloman719 <poloman7190@gmail.com>
2024-02-11 09:40:08 -0800

    not much

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,21 @@
 <script>
 	import '../app.css';
 	import NavItem from './NavItem.svelte';
+
+	const navItems = [{icon: "♟️", title: "Play"}]
 </script>
 
 <div class="flex">
 	<div class="h-screen w-40 m-0 flex flex-col bg-gray-100 text-white shadow-lg">
-		<NavItem icon="♟️" title="Play"/>
-		<NavItem icon="🧩" title="Puzzles"/>
-		<NavItem icon="🎓" title="Learn"/>
-		<NavItem icon="🥽" title="Watch"/>
-		<NavItem icon="📰" title="News"/>
-		<NavItem icon="🫂" title="Social"/>
-		<NavItem icon="😀" title="More"/>
+		<NavItem icon="♟️" title="Play" />
+		<NavItem icon="🧩" title="Puzzles" />
+		<NavItem icon="🎓" title="Learn" />
+		<NavItem icon="🥽" title="Watch" />
+		<NavItem icon="📰" title="News" />
+		<NavItem icon="🫂" title="Social" />
+		<NavItem icon="😀" title="More" />
 	</div>
+	<div class="bg-gray-25 fixed left-40 w-80 h-screen"></div>
 	<div class="bg-gray-200 flex-grow">
 		<slot />
 	</div>
