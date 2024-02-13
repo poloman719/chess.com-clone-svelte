4b4f1dacb86e8856f4d5c330d3814ed01e47c0dd
Jericho de Guzman <poloman7190@gmail.com>
2024-02-13 15:58:07 +0000

    idk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import '../app.css';
 	import logo from '$lib/logo.png';
 	import NavBar from './NavBar.svelte';
+
+	let search = 'Search';
 </script>
 
 <div class="flex">
@@ -11,7 +13,14 @@
 		</div>
 		<NavBar />
 		<div class="p-3 flex items-center justify-center">
-			<input type="text" class="bg-gray-400 w-full border border-gray-500 rounded-sm outline-none p-2 text-xs hover:border-gray-600 focus:border-gray-600" />
+			<input
+				bind:value={search}
+				on:focusin={() => (search = '')}
+				on:focusout={() => (search = 'Search')}
+				type="text"
+				class="bg-gray-400 w-full border border-gray-500 rounded-sm outline-none p-2 text-xs hover:border-gray-600 focus:border-gray-600"
+			/>
+			<div class="fixed translate-y-7 w-40 h-12 bg-gray-25"></div>
 		</div>
 	</div>
 	<div class="bg-gray-200 flex-grow">
